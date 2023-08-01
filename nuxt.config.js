fa6cac70a7499b1be63f856455e099136239bb4e
useing123 <useing@mail.ru>
2023-08-01 11:16:30 +0600

    attemp2

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxtjs/axios", "cookie-universal-nuxt"],
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    buildDir: 'dist',
+  },
 };
